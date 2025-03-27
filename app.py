4cc8e6e0bf681c3296147b91b472a55dc1bd4b63
Chukwuzara Nwachukwu <136655562+chnwa25@users.noreply.github.com>
2025-03-27 10:59:42 -0400

    Ready for deployment

--- app.py
+++ app.py
@@ -41,5 +41,7 @@ def predict():
         return jsonify({'error': str(e)})
 
 # Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
